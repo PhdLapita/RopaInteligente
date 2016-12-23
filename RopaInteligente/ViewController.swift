5b9ad532038cdb37d24fa00a88fe121b6ff39a06
Bear <bear@Bears-iMac.local>
2016-12-23 15:03:23 -0500

    Imágenes y primera pantalla

--- RopaInteligente/ViewController.swift
+++ RopaInteligente/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func clicVerMas(_ sender: UIButton) {
+        UIApplication.shared.openURL(NSURL(string: "http://www.bearcreekmining.com/s/Home.asp")! as URL)
+    }
 
 }
 
