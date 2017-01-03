c115260630fab7561fcd02cb7c717592a376e653
Lapita <glapav@gmail.com>
2017-01-02 23:18:55 -0500

    side bar listo

--- RopaInteligente/ViewController.swift
+++ RopaInteligente/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sidebar: NSLayoutConstraint!
+    
+    var menuShowing = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,6 +26,19 @@ class ViewController: UIViewController {
 
     @IBAction func clicVerMas(_ sender: UIButton) {
         UIApplication.shared.openURL(NSURL(string: "http://www.bearcreekmining.com/s/Home.asp")! as URL)
+    }
+    
+    @IBAction func clicMenu(_ sender: UIBarButtonItem) {
+        
+        if(menuShowing){
+                    sidebar.constant = -200
+        }else{
+            sidebar.constant = 0
+            UIView.animate(withDuration: 0.3, animations:{ self.view.layoutIfNeeded()})
+            
+        }
+        menuShowing = !menuShowing
+
     }
 
 }
